8b0c45356911e638be30c971c634a793459358ee
Peter Ban <bn.peter.hun@gmail.com>
2024-12-22 23:41:54 +0100

    Added buttons

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -12,6 +16,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.material3.Button
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +27,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    Buttons(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -38,10 +45,33 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+
+@Composable
+fun Buttons(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center, // Center vertically
+        horizontalAlignment = Alignment.CenterHorizontally // Center horizontally
+    ) {
+
+        Spacer(modifier = Modifier.height(8.dp)) // Add spacing between elements
+        Button(onClick = { /* Perform action */ }) {
+            Text(text = "Click Me")
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = { /* Perform action */ }) {
+            Text(text = "Click Me")
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        //Greeting("Android")
+        Buttons("Android")
     }
 }
